271733bc282957539398edd4fef0fcc93e5ae7b5
webdev498 <gcmobile@mail.com>
2017-04-28 15:06:55 +0800

    Mock up updates post review

--- front-end/components/Root.js
+++ front-end/components/Root.js
@@ -14,7 +14,7 @@ const Root = (): React.Element<Layout> =>
       </HeaderRow>
     </Header>
     <Content style={{padding: "32px"}}>
-      <h5>WILSON, Mark, Law Offices of Mark Wilson PMB: 348 2530 Berryessa Road San Jose, CA 95132</h5>
+      <h5>WILSON, Mark, Law Offices of Mark Wilson: 348 2530 Berryessa Road San Jose, CA 95132</h5>
       <DataTable style={{width: "100%"}}
                  rowKeyColumn="lawFirmId"
                  rows={[
@@ -41,12 +41,12 @@ const Root = (): React.Element<Layout> =>
         <TableHeader name="country">Country</TableHeader>
         <TableHeader name="serviceAddressId">Entity ID</TableHeader>
       </DataTable>
-      <Button raised style={{margin: "10px 10px 0 0"}}><Icon name="create" /> Create As New Firm</Button>
-      <Button raised style={{margin: "10px 10px 0 0"}}><Icon name="not_interested" /> Not a Law Firm</Button>
-      <Button raised style={{margin: "10px 10px 0 0"}}><Icon name="skip_next" /> Skip</Button>
+      <Button raised style={{margin: "10px 32px 0 0"}}><Icon name="create" /> Create As New Firm</Button>
+      <Button raised style={{margin: "10px 32px 0 0"}}><Icon name="not_interested" /> Not a Law Firm</Button>
+      <Button raised style={{margin: "10px 32px 0 0"}}><Icon name="skip_next" /> Skip</Button>
       <Button disabled accent style={{margin: "10px 10px 0 0", float: "right"}}><Icon name="description" /> View Sample Applications</Button>
 
-      <Tabs ripple style={{marginTop: "8px"}}>
+      <Tabs ripple style={{marginTop: "16px"}}>
         <Tab>Suggestions (US)</Tab>
         <Tab>Search</Tab>
       </Tabs>
@@ -57,30 +57,30 @@ const Root = (): React.Element<Layout> =>
                      {
                        lawFirmId: <span>58586 <IconButton accent name="open_in_new" /></span>,
                        entity: <a href="#">Law Offices Of Mark A. Wilson, CA</a>,
-                       serviceAddress: "WILSON, Mark, Law Offices of Mark Wilson PMB: 348 2530 Berryessa Road San Jose, CA 95132",
-                       website: <a href="#">http://www.wilsonlawaz.com/mark-wilson/</a>,
+                       serviceAddress: <p><span style={{background: "#ffffbc"}}>WILSON</span>, <span style={{background: "#ffffbc"}}>Mark</span>, <span style={{background: "#ffffbc"}}>Law</span> <span style={{background: "#ffffbc"}}>Offices</span> of <span style={{background: "#ffffbc"}}>Mark</span> <span style={{background: "#ffffbc"}}>Wilson</span> PMB: <span style={{background: "#ffffbc"}}>348</span> <span style={{background: "#ffffbc"}}>2530</span> <span style={{background: "#ffffbc"}}>Berryessa</span> Rd <span style={{background: "#ffffbc"}}>San</span> <span style={{background: "#ffffbc"}}>Jose</span>, CA <span style={{background: "#ffffbc"}}>95132</span><br /><span style={{background: "#ffffbc"}}>Mark</span> <span style={{background: "#ffffbc"}}>Wilson</span> PMB: <span style={{background: "#ffffbc"}}>348</span> <span style={{background: "#ffffbc"}}>2530</span> <span style={{background: "#ffffbc"}}>Berryessa</span> <span style={{background: "#ffffbc"}}>Road</span> <span style={{background: "#ffffbc"}}>San</span> <span style={{background: "#ffffbc"}}>Jose</span>, California <span style={{background: "#ffffbc"}}>95132</span></p>,
+                       website: <IconButton name="public" accent />,
                        serviceAddressId: "1036990",
                        actions: <IconButton name="cancel" accent />,
                      },
                      {
                        lawFirmId: <span style={{color: "#bbb"}}>Non law firm</span>,
-                       entity: <a href="#">NEL R. FONTANILLA</a>,
-                       serviceAddress: "1536 HEMMINGWAY ROAD, SAN JOSE CA 95132",
+                       entity: "NEL R. FONTANILLA",
+                       serviceAddress: <p>1536 HEMMINGWAY <span style={{background: "#ffffbc"}}>ROAD</span>, <span style={{background: "#ffffbc"}}>SAN</span> <span style={{background: "#ffffbc"}}>JOSE</span> <span style={{background: "#ffffbc"}}>CA</span> <span style={{background: "#ffffbc"}}>95132</span></p>,
                        serviceAddressId: "1051425",
                        actions: <IconButton name="cancel" accent />,
                      },
                      {
                        lawFirmId: <span style={{color: "#bbb"}}>Non law firm</span>,
-                       entity: <a href="#">INTERACTIV CORPORATION</a>,
-                       serviceAddress: "1659 N CAPITOL AVE # 225, 1659 N CAPITOL AVE # 225, SAN JOSE, CA 95132",
+                       entity: "INTERACTIV CORPORATION",
+                       serviceAddress: <p>1659 N CAPITOL AVE # 225, 1659 N CAPITOL AVE # 225, <span style={{background: "#ffffbc"}}>SAN</span> <span style={{background: "#ffffbc"}}>JOSE</span>, <span style={{background: "#ffffbc"}}>CA</span> <span style={{background: "#ffffbc"}}>95132</span></p>,
                        website: "",
                        serviceAddressId: "563472",
                        actions: <IconButton name="cancel" accent />,
                      },
                      {
                        lawFirmId: <span style={{color: "#bbb"}}>Non law firm</span>,
-                       entity: <a href="#">BEAUTYQQ INC</a>,
-                       serviceAddress: "2928 LAMBETH COURT, SAN JOSE, CA 95132",
+                       entity: "BEAUTYQQ INC",
+                       serviceAddress: <p>2928 LAMBETH COURT, <span style={{background: "#ffffbc"}}>SAN</span> <span style={{background: "#ffffbc"}}>JOSE</span>, <span style={{background: "#ffffbc"}}>CA</span> <span style={{background: "#ffffbc"}}>95132</span></p>,
                        website: "",
                        serviceAddressId: "584214",
                        actions: <IconButton name="cancel" accent />,
@@ -88,7 +88,7 @@ const Root = (): React.Element<Layout> =>
                      {
                        lawFirmId: <span>44161 <IconButton accent name="open_in_new" /></span>,
                        entity: <a href="#">PEREZ, YVONNE, CA</a>,
-                       serviceAddress: "PEREZ, YVONNE, 2121 LIMEWOOD DR., SAN JOSE, CA 95132",
+                       serviceAddress: <p>PEREZ, YVONNE, 2121 LIMEWOOD DR., <span style={{background: "#ffffbc"}}>SAN</span> <span style={{background: "#ffffbc"}}>JOSE</span>, <span style={{background: "#ffffbc"}}>CA</span> <span style={{background: "#ffffbc"}}>95132</span></p>,
                        website: "",
                        serviceAddressId: "610070",
                        actions: <IconButton name="cancel" accent />,
@@ -96,9 +96,9 @@ const Root = (): React.Element<Layout> =>
                    ]}
           >
           <TableHeader name="lawFirmId">Law Firm ID</TableHeader>
-          <TableHeader name="entity">Entity</TableHeader>
-          <TableHeader name="serviceAddress">Address</TableHeader>
-          <TableHeader name="website">Website</TableHeader>
+          <TableHeader name="entity">Firm</TableHeader>
+          <TableHeader name="serviceAddress">Service Addresses</TableHeader>
+          <TableHeader name="website">www</TableHeader>
           <TableHeader name="serviceAddressId">Entity ID</TableHeader>
           <TableHeader name="actions">Re-sort</TableHeader>
         </DataTable>
