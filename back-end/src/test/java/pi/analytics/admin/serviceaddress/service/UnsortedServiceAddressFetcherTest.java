a17f32582b18da1bef13be432f9511ee8ed483a5
webdev498 <gcmobile@mail.com>
2017-05-05 11:43:48 +0800

    Add more tests

--- back-end/src/test/java/pi/analytics/admin/serviceaddress/service/UnsortedServiceAddressFetcherTest.java
+++ back-end/src/test/java/pi/analytics/admin/serviceaddress/service/UnsortedServiceAddressFetcherTest.java
@@ -29,14 +29,17 @@ import pi.ip.generated.queue.MsgUnit;
 import pi.ip.generated.queue.QueueOnPremGrpc;
 import pi.ip.generated.queue.StoredMsgUnit;
 import pi.ip.generated.queue.UnitRequestOnPrem;
+import pi.ip.proto.generated.AckResponse;
 import pi.ip.proto.generated.ServiceAddress;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -89,11 +92,6 @@ public class UnsortedServiceAddressFetcherTest {
   }
 
   @Test
-  public void fetchNext() throws Exception {
-    // TODO
-  }
-
-  @Test
   public void fetchNextQueueItem_empty_queue() throws Exception {
     doAnswer(invocation -> {
       StreamObserver<StoredMsgUnit> responseObserver = (StreamObserver<StoredMsgUnit>) invocation.getArguments()[1];
@@ -126,12 +124,141 @@ public class UnsortedServiceAddressFetcherTest {
   }
 
   @Test
-  public void fetchNextQueueItem_found_invalid_service_address() throws Exception {
-    // TODO(SX)
+  public void fetchNextQueueItem_service_address_fetch_error_results_in_exception() throws Exception {
+    final StoredMsgUnit storedMsgUnit =
+        StoredMsgUnit
+            .newBuilder()
+            .setDbId("111")
+            .setMsgUnit(MsgUnit.newBuilder().setUniqueMsgKey("222"))
+            .build();
+
+    doAnswer(invocation -> {
+      StreamObserver<StoredMsgUnit> responseObserver = (StreamObserver<StoredMsgUnit>) invocation.getArguments()[1];
+      responseObserver.onNext(storedMsgUnit);
+      responseObserver.onCompleted();
+      return null;
+    })
+    .when(queueOnPrem)
+    .getNextQueueUnit(any(UnitRequestOnPrem.class), any(StreamObserver.class));
+
+    doAnswer(invocation -> {
+      StreamObserver<ServiceAddress> responseObserver = (StreamObserver<ServiceAddress>) invocation.getArguments()[1];
+      responseObserver.onError(new Exception());
+      return null;
+    })
+    .when(serviceAddressService)
+    .getServiceAddressById(any(GetServiceAddressByIdRequest.class), any(StreamObserver.class));
+
+    assertThatThrownBy(() -> { unsortedServiceAddressFetcher.fetchNext(faker.name().username()); })
+        .as("Service address fetch error results in an exception being thrown");
   }
 
   @Test
-  public void fetchNextQueueItem_found_valid_service_address() throws Exception {
+  public void fetchNextQueueItem_service_address_not_found() throws Exception {
+    final StoredMsgUnit storedMsgUnit =
+        StoredMsgUnit
+            .newBuilder()
+            .setDbId("111")
+            .setMsgUnit(MsgUnit.newBuilder().setUniqueMsgKey("222"))
+            .build();
+
+    doAnswer(invocation -> {
+      StreamObserver<StoredMsgUnit> responseObserver = (StreamObserver<StoredMsgUnit>) invocation.getArguments()[1];
+      responseObserver.onNext(storedMsgUnit);
+      responseObserver.onCompleted();
+      return null;
+    })
+    .when(queueOnPrem)
+    .getNextQueueUnit(any(UnitRequestOnPrem.class), any(StreamObserver.class));
+
+    doAnswer(invocation -> {
+      StreamObserver<AckResponse> responseObserver = (StreamObserver<AckResponse>) invocation.getArguments()[1];
+      responseObserver.onNext(AckResponse.getDefaultInstance());
+      responseObserver.onCompleted();
+      return null;
+    })
+    .when(queueOnPrem)
+    .deleteQueueUnit(any(DeleteUnitRequest.class), any(StreamObserver.class));
+
+    doAnswer(invocation -> {
+      StreamObserver<ServiceAddress> responseObserver = (StreamObserver<ServiceAddress>) invocation.getArguments()[1];
+      responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
+      return null;
+    })
+    .when(serviceAddressService)
+    .getServiceAddressById(any(GetServiceAddressByIdRequest.class), any(StreamObserver.class));
+
+    final Optional<QueuedServiceAddress> queuedServiceAddress =
+        unsortedServiceAddressFetcher.fetchNext("hellen");  // User has 5 queues
+
+    assertThat(queuedServiceAddress)
+        .as("Queues return an item whose service address cannot be found")
+        .isEmpty();
+
+    // Check that we delete the queue item for a service address that we can't find
+    final DeleteUnitRequest deleteUnitRequest = DeleteUnitRequest.newBuilder().setDbId(storedMsgUnit.getDbId()).build();
+    verify(queueOnPrem, times(5))  // Once per queue since we made all queues return the same item
+        .deleteQueueUnit(eq(deleteUnitRequest), any(StreamObserver.class));
+  }
+
+  @Test
+  public void fetchNextQueueItem_service_address_ignored_office_code() throws Exception {
+    final StoredMsgUnit storedMsgUnit =
+        StoredMsgUnit
+            .newBuilder()
+            .setDbId("111")
+            .setMsgUnit(MsgUnit.newBuilder().setUniqueMsgKey("222"))
+            .build();
+
+    doAnswer(invocation -> {
+      StreamObserver<StoredMsgUnit> responseObserver = (StreamObserver<StoredMsgUnit>) invocation.getArguments()[1];
+      responseObserver.onNext(storedMsgUnit);
+      responseObserver.onCompleted();
+      return null;
+    })
+    .when(queueOnPrem)
+    .getNextQueueUnit(any(UnitRequestOnPrem.class), any(StreamObserver.class));
+
+    doAnswer(invocation -> {
+      StreamObserver<AckResponse> responseObserver = (StreamObserver<AckResponse>) invocation.getArguments()[1];
+      responseObserver.onNext(AckResponse.getDefaultInstance());
+      responseObserver.onCompleted();
+      return null;
+    })
+    .when(queueOnPrem)
+    .deleteQueueUnit(any(DeleteUnitRequest.class), any(StreamObserver.class));
+
+    final ServiceAddress serviceAddress =
+        ServiceAddress
+            .newBuilder()
+            .setServiceAddressId(222L)
+            .setCountry("OFFICE_CODE_OF_NON_INTEREST")
+            .build();
+
+    doAnswer(invocation -> {
+      StreamObserver<ServiceAddress> responseObserver = (StreamObserver<ServiceAddress>) invocation.getArguments()[1];
+      responseObserver.onNext(serviceAddress);
+      responseObserver.onCompleted();
+      return null;
+    })
+    .when(serviceAddressService)
+    .getServiceAddressById(any(GetServiceAddressByIdRequest.class), any(StreamObserver.class));
+
+    final Optional<QueuedServiceAddress> queuedServiceAddress =
+        unsortedServiceAddressFetcher.fetchNext("user.with.4.queues");
+
+    assertThat(queuedServiceAddress)
+        .as("Queues return an item whose service address has an office code we are not interested in")
+        .isEmpty();
+
+    // Check that we delete the queue item for a service address that we don't care about
+    final DeleteUnitRequest deleteUnitRequest = DeleteUnitRequest.newBuilder().setDbId(storedMsgUnit.getDbId()).build();
+    verify(queueOnPrem, times(4))  // Once per queue since we made all queues return the same item
+        .deleteQueueUnit(eq(deleteUnitRequest), any(StreamObserver.class));
+  }
+
+  @Test
+  public void fetchNextQueueItem_valid_service_address() throws Exception {
     final StoredMsgUnit storedMsgUnit =
         StoredMsgUnit
             .newBuilder()
@@ -173,15 +300,5 @@ public class UnsortedServiceAddressFetcherTest {
         .isEqualTo(Optional.of(expectedResult));
 
     verify(queueOnPrem, never()).deleteQueueUnit(any(DeleteUnitRequest.class), any(StreamObserver.class));
-  }
-
-  @Test
-  public void fetchServiceAddress() throws Exception {
-    // TODO
-  }
-
-  @Test
-  public void pruneUnhandledQueueItem() throws Exception {
-    // TODO
   }
 }
