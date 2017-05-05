1d0302d115296654c906bf999d01dbe9545f8d32
webdev498 <gcmobile@mail.com>
2017-05-05 17:11:03 +0800

    Add Google Cloud Translation helper

---
+++ back-end/src/main/java/pi/analytics/admin/serviceaddress/service/TranslationHelper.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2017 Practice Insight Pty Ltd.
+ */
+
+package pi.analytics.admin.serviceaddress.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.Translate.TranslateOption;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.inject.Singleton;
+
+import java.io.IOException;
+
+import pi.ip.proto.generated.LangType;
+
+/**
+ * Translation helper service that uses the Google Cloud Translate API
+ * @author shane.xie@practiceinsight.io
+ */
+@Singleton
+public class TranslationHelper {
+
+  private Translate translate;
+
+  public TranslationHelper() {
+    try {
+      final GoogleCredentials credentials =
+          GoogleCredentials
+              .fromStream(
+                  getClass().getClassLoader().getResourceAsStream("pi-gcp-google-translate-service-account.json")
+              );
+      translate =
+          TranslateOptions
+              .newBuilder()
+              .setCredentials(credentials)
+              .build()
+              .getService();
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
+    }
+  }
+
+  public String toEn(final String text, final LangType sourceLanguage) {
+      return translate
+          .translate(
+              text,
+              TranslateOption.sourceLanguage(getLanguageString(sourceLanguage)),
+              TranslateOption.targetLanguage("en")
+          )
+          .getTranslatedText();
+  }
+
+  private String getLanguageString(final LangType langType) {
+    switch (langType) {
+      case WESTERN_SCRIPT:
+        return "en";
+      case JAPANESE:
+        return "ja";
+      case CHINESE:
+        return "zh";
+      case KOREAN:
+        return "ko";
+      case CYRILLIC:
+        return "ru";
+      default:
+        throw new IllegalArgumentException("Unknown LangType");
+    }
+  }
+}
---
+++ back-end/src/test/java/pi/analytics/admin/serviceaddress/service/TranslationHelperIntegrationTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2017 Practice Insight Pty Ltd.
+ */
+package pi.analytics.admin.serviceaddress.service;
+
+import com.pi.common.test.type.DisabledTests;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import pi.ip.proto.generated.LangType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author shane.xie@practiceinsight.io
+ */
+public class TranslationHelperIntegrationTest {
+
+  @Category(DisabledTests.class)
+  @Test()
+  public void toEn() throws Exception {
+    final TranslationHelper translationHelper = new TranslationHelper();
+    assertThat(translationHelper.toEn("武士", LangType.JAPANESE)).isEqualTo("samurai");
+  }
+}
