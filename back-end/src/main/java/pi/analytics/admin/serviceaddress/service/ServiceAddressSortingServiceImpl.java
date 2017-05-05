d97da62fa4ca764c9b3a45697c950a3bd4ef1266
webdev498 <gcmobile@mail.com>
2017-05-05 17:15:15 +0800

    WIP: Next unsorted service address RPC

--- back-end/src/main/java/pi/analytics/admin/serviceaddress/service/ServiceAddressSortingServiceImpl.java
+++ back-end/src/main/java/pi/analytics/admin/serviceaddress/service/ServiceAddressSortingServiceImpl.java
@@ -7,6 +7,12 @@ package pi.analytics.admin.serviceaddress.service;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import pi.admin.service_address_sorting.generated.AssignServiceAddressRequest;
 import pi.admin.service_address_sorting.generated.CreateLawFirmRequest;
@@ -21,6 +27,7 @@ import pi.admin.service_address_sorting.generated.ServiceAddressSortingServiceGr
 import pi.admin.service_address_sorting.generated.ServiceAddressUnsorted;
 import pi.admin.service_address_sorting.generated.SetServiceAddressAsNonLawFirmRequest;
 import pi.admin.service_address_sorting.generated.UnsortServiceAddressRequest;
+import pi.ip.proto.generated.LangType;
 
 /**
  * @author shane.xie@practiceinsight.io
@@ -28,17 +35,36 @@ import pi.admin.service_address_sorting.generated.UnsortServiceAddressRequest;
 @Singleton
 public class ServiceAddressSortingServiceImpl extends ServiceAddressSortingServiceGrpc.ServiceAddressSortingServiceImplBase {
 
+  private static final Logger log = LoggerFactory.getLogger(ServiceAddressSortingServiceImpl.class);
+
   @Inject
   private UnsortedServiceAddressFetcher unsortedServiceAddressFetcher;
+
+  @Inject
+  TranslationHelper translationHelper;
 
   @Override
   public void nextUnsortedServiceAddress(final NextUnsortedServiceAddressRequest request,
                                          final StreamObserver<ServiceAddressBundle> responseObserver) {
-//    unsortedServiceAddressFetcher.fetchNext(request.getRequestedBy())
-//        .filter(queuedServiceAddress -> queuedServiceAddress.serviceAddress().isPresent())
-//        .map(queuedServiceAddress -> {
-//          // Get translation if not en
-//        });
+    try {
+      final Optional<ServiceAddressBundle> serviceAddressBundle =
+          unsortedServiceAddressFetcher
+              .fetchNext(request.getRequestedBy())
+              .filter(queuedServiceAddress -> queuedServiceAddress.serviceAddress().isPresent())
+              .map(this::createServiceAddressBundle)
+              .map(this::addTranslationIfNecessary)
+              .map(this::addAgentSuggestions);
+
+      if (!serviceAddressBundle.isPresent()) {
+        // Nothing in queues
+        responseObserver.onError(Status.NOT_FOUND.asRuntimeException());
+      }
+      responseObserver.onNext(serviceAddressBundle.get());
+      responseObserver.onCompleted();
+    } catch (Throwable th) {
+      log.error("Error generating next unsorted service address", th);
+      responseObserver.onError(th);
+    }
   }
 
   @Override
@@ -67,5 +93,31 @@ public class ServiceAddressSortingServiceImpl extends ServiceAddressSortingServi
   public void setServiceAddressAsNonLawFirm(final SetServiceAddressAsNonLawFirmRequest request,
                                             final StreamObserver<ServiceAddressSetAsNonLawFirm> responseObserver) {
     // TODO(SX)
+  }
+
+  private ServiceAddressBundle createServiceAddressBundle(final QueuedServiceAddress queuedServiceAddress) {
+    return ServiceAddressBundle
+        .newBuilder()
+        .setServiceAddressToSort(queuedServiceAddress.serviceAddress().get())
+        .setUnsortedServiceAddressQueueItemId(queuedServiceAddress.queueId())
+        .build();
+  }
+
+  private ServiceAddressBundle addTranslationIfNecessary(final ServiceAddressBundle bundle) {
+    if (bundle.getServiceAddressToSort().getLanguageType() == LangType.WESTERN_SCRIPT) {
+      // No translation required
+      return bundle;
+    }
+    final LangType sourceLanguage = bundle.getServiceAddressToSort().getLanguageType();
+    final String textToTranslate = bundle.getServiceAddressToSort().getAddress();
+    return ServiceAddressBundle
+        .newBuilder(bundle)
+        .setEnTranslation(translationHelper.toEn(textToTranslate, sourceLanguage))
+        .build();
+  }
+
+  private ServiceAddressBundle addAgentSuggestions(final ServiceAddressBundle bundle) {
+    // TODO(SX)
+    return bundle;
   }
 }
