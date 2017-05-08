b8a0dd0dd0fa362b3f1671407985e4abe626175f
webdev498 <gcmobile@mail.com>
2017-05-08 16:09:45 +0800

    Add LawFirmDbService configuration

---
+++ back-end/src/main/java/pi/analytics/admin/serviceaddress/guice/LawFirmDbServiceBlockingStubProvider.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017 Practice Insight Pty Ltd.
+ */
+
+package pi.analytics.admin.serviceaddress.guice;
+
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+import com.pi.common.config.PiConfig;
+import com.pi.common.config.PiKubeServiceImpl;
+import com.pi.common.config.PiKubeServicePort;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import pi.ip.data.relational.generated.LawFirmDbServiceGrpc;
+
+/**
+ * @author shane.xie@practiceinsight.io
+ */
+@Singleton
+public class LawFirmDbServiceBlockingStubProvider implements Provider<LawFirmDbServiceGrpc.LawFirmDbServiceBlockingStub> {
+
+  @Override
+  public LawFirmDbServiceGrpc.LawFirmDbServiceBlockingStub get() {
+    final PiConfig piConfig = PiConfig.get();
+    final ManagedChannel channel =
+        ManagedChannelBuilder
+            .forAddress(
+                piConfig.getServiceHostname(PiKubeServiceImpl.IP_DATA_RELATIONAL_SERVICE_HOST),
+                piConfig.getInteger(PiKubeServicePort.IP_DATA_RELATIONAL_SERVICE_PORT)
+            )
+            .build();
+    return LawFirmDbServiceGrpc.newBlockingStub(channel);
+  }
+}
--- back-end/src/main/java/pi/analytics/admin/serviceaddress/guice/ServiceAddressSortingModule.java
+++ back-end/src/main/java/pi/analytics/admin/serviceaddress/guice/ServiceAddressSortingModule.java
@@ -7,6 +7,7 @@ package pi.analytics.admin.serviceaddress.guice;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
+import pi.ip.data.relational.generated.LawFirmDbServiceGrpc;
 import pi.ip.data.relational.generated.ServiceAddressServiceGrpc;
 import pi.ip.generated.datastore_sg3.DatastoreSg3ServiceGrpc;
 import pi.ip.generated.queue.QueueOnPremGrpc;
@@ -20,6 +21,9 @@ public class ServiceAddressSortingModule extends AbstractModule {
   protected void configure() {
     bind(QueueOnPremGrpc.QueueOnPremBlockingStub.class)
         .toProvider(QueueOnPremBlockingStubProvider.class)
+        .in(Singleton.class);
+    bind(LawFirmDbServiceGrpc.LawFirmDbServiceBlockingStub.class)
+        .toProvider(LawFirmDbServiceBlockingStubProvider.class)
         .in(Singleton.class);
     bind(DatastoreSg3ServiceGrpc.DatastoreSg3ServiceBlockingStub.class)
         .toProvider(DatastoreSg3BlockingStubProvider.class)
