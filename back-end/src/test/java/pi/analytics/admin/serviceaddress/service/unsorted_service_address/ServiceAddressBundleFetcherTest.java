309dca170881fa08d5d227e4596fb1669e2cb2c4
webdev498 <gcmobile@mail.com>
2017-05-09 22:26:28 +0800

    Add tests for add translation if necessary

--- back-end/src/test/java/pi/analytics/admin/serviceaddress/service/unsorted_service_address/ServiceAddressBundleFetcherTest.java
+++ back-end/src/test/java/pi/analytics/admin/serviceaddress/service/unsorted_service_address/ServiceAddressBundleFetcherTest.java
@@ -25,11 +25,14 @@ import pi.analytics.admin.serviceaddress.service.QueuedServiceAddress;
 import pi.ip.data.relational.generated.LawFirmDbServiceGrpc;
 import pi.ip.data.relational.generated.ServiceAddressServiceGrpc;
 import pi.ip.generated.datastore_sg3.DatastoreSg3ServiceGrpc;
+import pi.ip.proto.generated.LangType;
 import pi.ip.proto.generated.ServiceAddress;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author shane.xie@practiceinsight.io
@@ -40,17 +43,17 @@ public class ServiceAddressBundleFetcherTest {
   private LawFirmDbServiceGrpc.LawFirmDbServiceImplBase lawFirmDbService;
   private ServiceAddressServiceGrpc.ServiceAddressServiceImplBase serviceAddressService;
   private DatastoreSg3ServiceGrpc.DatastoreSg3ServiceImplBase datastoreSg3Service;
+  private TranslationHelper translationHelper;
   private Server server;
   private ManagedChannel channel;
   private ServiceAddressBundleFetcher serviceAddressBundleFetcher;
-
-  TranslationHelper translationHelper;
 
   @Before
   public void setUp() throws Exception {
     lawFirmDbService = mock(LawFirmDbServiceGrpc.LawFirmDbServiceImplBase.class);
     serviceAddressService = mock(ServiceAddressServiceGrpc.ServiceAddressServiceImplBase.class);
     datastoreSg3Service = mock(DatastoreSg3ServiceGrpc.DatastoreSg3ServiceImplBase.class);
+    translationHelper = mock(TranslationHelper.class);
 
     final String serverName = "service-address-bundle-fetcher-test-".concat(UUID.randomUUID().toString());
     server =
@@ -77,6 +80,8 @@ public class ServiceAddressBundleFetcherTest {
             .toInstance(ServiceAddressServiceGrpc.newBlockingStub(channel));
         bind(DatastoreSg3ServiceGrpc.DatastoreSg3ServiceBlockingStub.class)
             .toInstance(DatastoreSg3ServiceGrpc.newBlockingStub(channel));
+        bind(TranslationHelper.class)
+            .toInstance(translationHelper);
       }
     }).getInstance(ServiceAddressBundleFetcher.class);
   }
@@ -116,8 +121,61 @@ public class ServiceAddressBundleFetcherTest {
   }
 
   @Test
-  public void addTranslationIfNecessary() throws Exception {
-    // TODO(SX)
+  public void addTranslationIfNecessary_skipped_no_source_language() throws Exception {
+    final ServiceAddressBundle bundle =
+        ServiceAddressBundle
+            .newBuilder()
+            .setServiceAddressToSort(ServiceAddress.getDefaultInstance())
+            .build();
+    assertThat(bundle)
+        .isEqualTo(serviceAddressBundleFetcher.addTranslationIfNecessary.apply(bundle));
+  }
+
+  @Test
+  public void addTranslationIfNecessary_skipped_western_script() throws Exception {
+    final ServiceAddressBundle bundle =
+        ServiceAddressBundle
+            .newBuilder()
+            .setServiceAddressToSort(
+                ServiceAddress
+                    .newBuilder()
+                    .setLanguageType(LangType.WESTERN_SCRIPT)
+                    .build()
+            )
+            .build();
+    assertThat(bundle)
+        .isEqualTo(serviceAddressBundleFetcher.addTranslationIfNecessary.apply(bundle));
+  }
+
+  @Test
+  public void addTranslationIfNecessary_translated() throws Exception {
+    final String sourceText = "source-text";
+    final String translatedText = "translated-text";
+
+    final ServiceAddress serviceAddress =
+        ServiceAddress
+            .newBuilder()
+            .setLanguageType(LangType.JAPANESE)
+            .setAddress(sourceText)
+            .build();
+
+    final ServiceAddressBundle sourceBundle =
+        ServiceAddressBundle
+            .newBuilder()
+            .setServiceAddressToSort(serviceAddress)
+            .build();
+
+    final ServiceAddressBundle translatedBundle =
+        ServiceAddressBundle
+            .newBuilder(sourceBundle)
+            .setEnTranslation(translatedText)
+            .build();
+
+    when(translationHelper.toEn(eq(sourceText), eq(LangType.JAPANESE)))
+        .thenReturn(translatedText);
+
+    assertThat(translatedBundle)
+        .isEqualTo(serviceAddressBundleFetcher.addTranslationIfNecessary.apply(sourceBundle));
   }
 
   @Test
