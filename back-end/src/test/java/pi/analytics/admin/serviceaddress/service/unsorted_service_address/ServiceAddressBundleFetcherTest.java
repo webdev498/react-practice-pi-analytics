44848e1ef29d0fa8ef968510cb865cbb848959ed
webdev498 <gcmobile@mail.com>
2017-05-09 17:25:41 +0800

    Add create service address bundle tests

--- back-end/src/test/java/pi/analytics/admin/serviceaddress/service/unsorted_service_address/ServiceAddressBundleFetcherTest.java
+++ back-end/src/test/java/pi/analytics/admin/serviceaddress/service/unsorted_service_address/ServiceAddressBundleFetcherTest.java
@@ -13,15 +13,23 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import io.grpc.ManagedChannel;
 import io.grpc.Server;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
+import pi.admin.service_address_sorting.generated.ServiceAddressBundle;
+import pi.analytics.admin.serviceaddress.service.QueuedServiceAddress;
 import pi.ip.data.relational.generated.LawFirmDbServiceGrpc;
 import pi.ip.data.relational.generated.ServiceAddressServiceGrpc;
 import pi.ip.generated.datastore_sg3.DatastoreSg3ServiceGrpc;
+import pi.ip.proto.generated.ServiceAddress;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author shane.xie@practiceinsight.io
@@ -40,6 +48,10 @@ public class ServiceAddressBundleFetcherTest {
 
   @Before
   public void setUp() throws Exception {
+    lawFirmDbService = mock(LawFirmDbServiceGrpc.LawFirmDbServiceImplBase.class);
+    serviceAddressService = mock(ServiceAddressServiceGrpc.ServiceAddressServiceImplBase.class);
+    datastoreSg3Service = mock(DatastoreSg3ServiceGrpc.DatastoreSg3ServiceImplBase.class);
+
     final String serverName = "service-address-bundle-fetcher-test-".concat(UUID.randomUUID().toString());
     server =
         InProcessServerBuilder
@@ -76,8 +88,31 @@ public class ServiceAddressBundleFetcherTest {
   }
 
   @Test
+  public void createServiceAddressBundle_fails_no_service_address() throws Exception {
+    final QueuedServiceAddress queuedServiceAddress = QueuedServiceAddress
+        .create(faker.numerify("#####"), Optional.empty());
+    assertThatThrownBy(() -> { serviceAddressBundleFetcher.createServiceAddressBundle.apply(queuedServiceAddress); })
+        .isInstanceOf(IllegalArgumentException.class)
+        .as("Service address cannot be empty");
+  }
+
+  @Test
   public void createServiceAddressBundle() throws Exception {
-    // TODO(SX)
+    final String queueId = faker.numerify("#####");
+    final ServiceAddress serviceAddress =
+        ServiceAddress
+            .newBuilder()
+            .setServiceAddressId(faker.number().randomNumber())
+            .build();
+    final QueuedServiceAddress queuedServiceAddress = QueuedServiceAddress.create(queueId, Optional.of(serviceAddress));
+    final ServiceAddressBundle serviceAddressBundle =
+        ServiceAddressBundle
+            .newBuilder()
+            .setUnsortedServiceAddressQueueItemId(queueId)
+            .setServiceAddressToSort(serviceAddress)
+            .build();
+    assertThat(serviceAddressBundle)
+        .isEqualTo(serviceAddressBundleFetcher.createServiceAddressBundle.apply(queuedServiceAddress));
   }
 
   @Test
