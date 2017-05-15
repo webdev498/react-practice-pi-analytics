dde5ad68f2f5dfcc0a160b297c3ee9f133334e86
webdev498 <gcmobile@mail.com>
2017-05-15 11:53:05 +0800

    Workaround for failing build because of uglify error

--- front-end/src/webpack.production.config.js
+++ front-end/src/webpack.production.config.js
@@ -18,7 +18,7 @@ module.exports = {
     }),
     // new webpack.optimize.CommonsChunkPlugin('common.js'),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
+    // new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
   ],
