219a46039ccedc50ee0e0e9fe24d82e2d7b66134
webdev498 <gcmobile@mail.com>
2017-05-29 17:10:36 +0800

    FA-352 Link to law firm detail does not work (service address sorting)

--- front-end/src/services/Urls.js
+++ front-end/src/services/Urls.js
@@ -18,7 +18,7 @@ export const ApiUrls = {
 export const OuterUrls = {
   sessionInfo: "http://practiceinsight.io/int/addressing/sessionInfo.jsp",
   login: "http://practiceinsight.io/int/login.jsp#agents",
-  firmInfo: "http://practiceinsight.io/int/addressing/dataServices/firmInfo.jsp?id=",
+  firmInfo: "http://practiceinsight.io/int/dataServices/firmInfo.jsp?id=",
   googleSearch: "https://www.google.com/search?q=",
   country: "https://practiceinsight.io/int/addressing/localCountryC.jsp?findLocalEntity=Find&localID="
 }
